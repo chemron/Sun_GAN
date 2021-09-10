58dbacd2cbd2fd32e7a39fe21f33ef37cda9e094
chemron <camerontasmith@gmail.com>
2021-09-10 15:19:06 +1000

    copy from uv gan script

---
+++ Scripts/train_seismic_GAN.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_seismic
+
+# Request CPU resource for a serial job
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+# Request for GPU,
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:P100:1
+# SBATCH --gres=gpu:K80:1
+
+# Memory usage (MB)
+#SBATCH --mem=10G
+
+# Set your minimum acceptable walltime, format: day-hours:minutes:seconds
+#SBATCH --time=90:00:00
+# SBATCH --time=00:00:20
+
+#SBATCH --mail-user=csmi0005@student.monash.edu
+#SBATCH --mail-type=FAIL
+
+module load anaconda/5.1.0-Python3.6-gcc5
+module load cudnn/7.6.5-cuda10.1
+module load tensorflow/2.3.0
+
+python Training/train.py \
+    --model_name "UV_GAN_1" \
+    --display_iter 5 \
+    --max_iter 20 \
+    --batch_size 1 \
+    --tol 3 \
+    --input "aia.np_path_normal" \
+    --output "hmi.np_path_normal" \
+    --connector "aia.id" "hmi.aia_id" \
+# input: sql data name (table.column)
+# output: sql data name (table.column)
+# connector: the connection between the input and output
