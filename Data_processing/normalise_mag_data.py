4515fc6ac9825b0b8796acfccd60a0b6fa911a11
chemron <camerontasmith@gmail.com>
2021-09-16 10:47:14 +1000

    mask images

--- Data_processing/normalise_mag_data.py
+++ Data_processing/normalise_mag_data.py
@@ -7,6 +7,19 @@ q = [0, 0.01, 0.1, 1, 5, 10, 25, 50, 75, 90, 95, 99, 99.9, 99.99, 100]
 percentiles = np.load(f"Data/np_objects/HMI_percentiles.npy").T
 w = h = 1024
 
+
+def get_mask(size):
+    w = h = size
+    center = (int(w/2), int(h/2))
+    radius = w/2 + 1
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+    mask = dist_from_center <= radius
+    return mask
+
+
+mask = get_mask(w)
+
 abs_max = abs(np.max(percentiles[-1]))
 abs_min = abs(np.min(percentiles[0]))
 
@@ -14,10 +27,13 @@ print(f"Absolute max: {abs_max}, Absolute min: {abs_min}")
 
 clip_max = np.max([abs_max, -abs_min])
 
+
 np_dir = f"Data/np_HMI/"
 normal_np_dir = f"Data/np_HMI_normalised/"
 if not os.path.exists(normal_np_dir):
     os.makedirs(normal_np_dir)
+
+
 
 data = np.sort(os.listdir(np_dir))
 
@@ -41,6 +57,8 @@ for i in range(len(data)):
 
     try:
         img = cv2.resize(img, dsize=(w, h))
+        img *= mask
+
         np.save(normal_np_dir + name, img)
         percentiles = np.percentile(img, q)
         if percentiles is not None:
@@ -54,6 +72,6 @@ for i in range(len(data)):
 
 percentile_dir = "Data/np_objects/"
 os.makedirs(percentile_dir) if not os.path.exists(percentile_dir) else None
-
+np.save(f"{percentile_dir}HMI_clip_max", clip_max)
 np.save(f"{percentile_dir}HMI_normal_percentiles", normal_p)
 np.save(f"{percentile_dir}HMI_normal_dates", normal_d)
